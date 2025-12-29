00580472618d429dca1ada39a657e76a32bc937b
Ziyad Eslam <ziyadeslam@hotmail.com>
2025-12-30 00:06:39 +0200

    feat: implement metadata generation for short links

--- client/app/[slug1]/[slug2]/page.tsx
+++ client/app/[slug1]/[slug2]/page.tsx
@@ -1,10 +1,24 @@
+import { Metadata } from 'next';
 import { redirect } from 'next/navigation';
+
+import { generateMetadata as generateAvailabilityMetadata } from '@/app/meeting/[meetingId]/availability/[availabilityId]/page';
 
 export interface Props {
   params: Promise<{
     slug1: string;
     slug2: string;
   }>;
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { slug1, slug2 } = await params;
+
+  if (slug1.length === 6 && slug2.length === 16) {
+    return generateAvailabilityMetadata({
+      params: Promise.resolve({ meetingId: slug1, availabilityId: slug2 }),
+    });
+  }
+  return {};
 }
 
 export default async function SecondRedirect({ params }: Props) {
--- client/app/[slug1]/page.tsx
+++ client/app/[slug1]/page.tsx
@@ -1,9 +1,29 @@
+import { Metadata } from 'next';
 import { redirect } from 'next/navigation';
+
+import { generateMetadata as generateManageMetadata } from '@/app/manage/[privateMeetingId]/page';
+import { generateMetadata as generateMeetingMetadata } from '@/app/meeting/[meetingId]/page';
 
 export interface Props {
   params: Promise<{
     slug1: string;
   }>;
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { slug1 } = await params;
+
+  if (slug1.length === 6) {
+    return generateMeetingMetadata({
+      params: Promise.resolve({ meetingId: slug1 }),
+    });
+  }
+  if (slug1.length === 12) {
+    return generateManageMetadata({
+      params: Promise.resolve({ privateMeetingId: slug1 }),
+    });
+  }
+  return {};
 }
 
 export default async function FirstRedirect({ params }: Props) {
